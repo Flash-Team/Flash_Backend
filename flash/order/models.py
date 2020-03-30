5d19c1f76e6460946457ee5ce60067885e67f189
mebr0 <aza17e@gmail.com>
2020-03-30 20:34:47 +0600

    Add order app views

--- flash/order/models.py
+++ flash/order/models.py
@@ -1,23 +1,22 @@
 from django.db import models
-from django.utils import timezone
+
 # noinspection PyProtectedMember
 from flash._auth.models import MyUser
 from flash.organization.models import Filial
 from flash.product.models import Product
-from django.core.validators import MinValueValidator
 
 
 class Order(models.Model):
     address = models.CharField(max_length=300)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    filial = models.ForeignKey(Filial, on_delete=models.CASCADE, default=1)
+    filial = models.ForeignKey(Filial, on_delete=models.CASCADE)
     client = models.ForeignKey(MyUser, on_delete=models.CASCADE, related_name='client')
     courier = models.ForeignKey(MyUser, on_delete=models.CASCADE, related_name='courier')
     delivered = models.BooleanField(default=False)
-    created_at = models.DateTimeField(default=timezone.now)
+    created_at = models.DateTimeField(auto_now=True)
 
 
 class OrderedProduct(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    count = models.PositiveIntegerField(default=1, validators=[MinValueValidator])
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='products')
+    count = models.IntegerField(default=1)
     order = models.ForeignKey(Order, on_delete=models.CASCADE)

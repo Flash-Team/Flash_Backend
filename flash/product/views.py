ed28f18d171172b0ad0c4873ba56947a55f211c4
shynaraaya <ayanbekshynar@gmail.com>
2020-04-27 08:14:07 +0600

    Views: fbv and cbv added

--- flash/product/views.py
+++ flash/product/views.py
@@ -1,5 +1,10 @@
+from django.http import Http404
 from rest_framework import viewsets
+from rest_framework.decorators import api_view
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
 
 from flash.product.models import Category, Product
 from flash.product.serializers import CategorySerializer, ProductSerializer, NestedProductSerializer
@@ -52,3 +57,45 @@ class ProductsListViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         category_id = self.kwargs.get('parent_lookup_category')
         serializer.save(category=Category.objects.get(id=category_id))
+
+
+@api_view(['GET', 'POST'])
+def category_list(request):
+    if request.method == 'GET':
+        categories = Category.objects.all()
+        serializer = CategorySerializer(categories, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = CategorySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response({'error': serializer.errors},
+                        status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+class CategoryDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Category.objects.get(pk=pk)
+        except Category.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        category = self.get_object(pk)
+        serializer = CategorySerializer(category)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        category = self.get_object(pk)
+        serializer = CategorySerializer(category, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        category = self.get_object(pk)
+        category.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
